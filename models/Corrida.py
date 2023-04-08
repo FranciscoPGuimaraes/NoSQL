b60e3c6b1c0ac1914f82ba847790d335f70cca60
Francisco Pereira Guimarães <111232681+FranciscoPGuimaraes@users.noreply.github.com>
2023-04-07 21:50:09 -0300

    Delete models directory

--- models/Corrida.py
+++
@@ -1,9 +0,0 @@
-from Passageiro import Passageiro
-
-
-class Corrida:
-    def __init__(self, nota, distancia, valor, passageiro: Passageiro):
-        self.nota = nota
-        self.distancia = distancia
-        self.valor = valor
-        self.passageiro = passageiro
--- models/Motorista.py
+++
@@ -1,9 +0,0 @@
-from Corrida import Corrida
-
-
-class Motorista:
-    corridas = []
-
-    def __init__(self, nota, corridas):
-        self.nota = nota
-        self.corridas = corridas
