4435f2c6f95592c60f7ae18edac069b67f314608
Francisco Pereira Guimarães <111232681+FranciscoPGuimaraes@users.noreply.github.com>
2023-04-07 21:49:39 -0300

    Add files via upload

---
+++ models/Corrida.py
@@ -0,0 +1,9 @@
+from Passageiro import Passageiro
+
+
+class Corrida:
+    def __init__(self, nota, distancia, valor, passageiro: Passageiro):
+        self.nota = nota
+        self.distancia = distancia
+        self.valor = valor
+        self.passageiro = passageiro
---
+++ models/Motorista.py
@@ -0,0 +1,9 @@
+from Corrida import Corrida
+
+
+class Motorista:
+    corridas = []
+
+    def __init__(self, nota, corridas):
+        self.nota = nota
+        self.corridas = corridas
